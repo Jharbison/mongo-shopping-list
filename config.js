5dd302b2ab6b74ff945e0066f08e9450043eadf7
Jharbison <jeff.p.harbison@gmail.com>
2016-10-12 22:23:30 -0400

    new

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://<jeffharbison>:<21seymour>@ds027825.mlab.com:27825/shopping-list');
 exports.PORT = process.env.PORT || 8080;
