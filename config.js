05f8452b4ac58486b8300b01366c764cae83d946
Jharbison <jeff.p.harbison@gmail.com>
2016-10-13 20:28:53 -0400

    fix mlab 3

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://jeffharbison:21seymour@ds027825.mlab.com:27825/shopping-list';
+                            'mongodb://jeffharbison:21seymour@ds027825.mlab.com:27825/shopping-list':
+                            'mongodb://localhost/shopping-list');
 exports.PORT = process.env.PORT || 8080;
